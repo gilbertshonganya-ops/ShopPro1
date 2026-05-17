16958b28e26c350f935f98bc60a15fcd3d971818
Gilbertpro1 <gilbertshonganya@gmail.com>
2026-05-17 14:05:23 +0100

    script.js

--- script.js
+++ script.js
@@ -22,3 +22,27 @@ document.getElementById("cart-icon").addEventListener("click", () => {
 
 // INIT
 updateCartIcon();
+
+//resultat de la barre de rechercher
+
+function searchProducts(){
+  let value=document.getElementById("searchInput").value.toLowerCase();
+  let container=document.getElementById("search-results");
+  container.innerHTML="";
+  if(value==="") return;
+  let results=allProducts.filter(product=>product.name.toLowerCase().includes(value));
+  
+  results.forEach(product=>{
+    container.innerHTML+=`<div class="result-item"onclick="window.location.href='${product.page}'">
+    <img src="${product.image}">
+    
+    <div>
+      <h4>${product.name}</h4>
+      <p>${product.price}</p>
+    </div>
+
+</div>`;
+
+});
+
+}
